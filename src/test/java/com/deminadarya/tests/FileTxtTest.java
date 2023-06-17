f2afdbf849bf0a43f1322d6918e5d622338d9b32
daria <darya.demina@simbirsoft.com>
2023-06-17 03:36:49 +0300

    add beforeAll

--- src/test/java/com/deminadarya/tests/FileTxtTest.java
+++ src/test/java/com/deminadarya/tests/FileTxtTest.java
@@ -1,7 +1,9 @@
 package com.deminadarya.tests;
 
+import com.codeborne.selenide.Configuration;
 import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -13,24 +15,30 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FileTxtTest {
 
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://github.com";
+        Configuration.pageLoadTimeout = 60000;
+    }
+
     @Test
     void downloadFile() throws IOException {
-        open("https://github.com/microsoft/pict/blob/main/doc/sample-models/create_volume.txt");
+        open("/microsoft/pict/blob/main/doc/sample-models/create_volume.txt");
         File downloadFile = $("#raw-url").download();
         Assertions.assertTrue(downloadFile.isFile());
     }
 
     @Test
     void shouldHaveContent() throws IOException {
-        open("https://github.com/microsoft/pict/blob/main/doc/sample-models/create_volume.txt");
+        open("/microsoft/pict/blob/main/doc/sample-models/create_volume.txt");
         File downloadFile = $("#raw-url").download();
         String fileContent = FileUtils.readFileToString(downloadFile, "UTF-8");
-        Assertions.assertTrue(fileContent.contains("Create and format  volume"));
+        Assertions.assertTrue(fileContent.contains("Create and format a volume"));
     }
 
     @Test
     void shouldHaveNameFile() throws IOException {
-        open("https://github.com/microsoft/pict/blob/main/doc/sample-models/create_volume.txt");
+        open("/microsoft/pict/blob/main/doc/sample-models/create_volume.txt");
         File downloadFile = $("#raw-url").download();
         String nameFile = downloadFile.getName();
         assertEquals(nameFile, "create_volume.txt");
