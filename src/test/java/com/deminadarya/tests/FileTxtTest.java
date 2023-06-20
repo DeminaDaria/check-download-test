76bb12dad73707169c8b992e7d2921128ceca50f
daria <darya.demina@simbirsoft.com>
2023-06-20 06:45:32 +0300

    add tests for txt file

--- src/test/java/com/deminadarya/tests/FileTxtTest.java
+++ src/test/java/com/deminadarya/tests/FileTxtTest.java
@@ -1,46 +1,32 @@
 package com.deminadarya.tests;
 
-import com.codeborne.selenide.Configuration;
 import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
 
 public class FileTxtTest {
 
-    @BeforeAll
-    static void beforeAll() {
-        Configuration.baseUrl = "https://github.com";
-        Configuration.pageLoadTimeout = 60000;
-    }
-
-    @Test
-    void downloadFile() throws IOException {
-        open("/microsoft/pict/blob/main/doc/sample-models/create_volume.txt");
-        File downloadFile = $("#raw-url").download();
-        Assertions.assertTrue(downloadFile.isFile());
-    }
-
     @Test
     void shouldHaveContent() throws IOException {
-        open("/microsoft/pict/blob/main/doc/sample-models/create_volume.txt");
-        File downloadFile = $("#raw-url").download();
-        String fileContent = FileUtils.readFileToString(downloadFile, "UTF-8");
-        Assertions.assertTrue(fileContent.contains("Create and format a volume"));
+        String txtFilePath = "src/test/resources/files/1.txt";
+        String fileContent = FileUtils.readFileToString(new File(txtFilePath), "UTF-8");
+        String expectedContent = "Create and format a volume";
+
+        Assertions.assertTrue(fileContent.contains(expectedContent));
     }
 
     @Test
-    void shouldHaveNameFile() throws IOException {
-        open("/microsoft/pict/blob/main/doc/sample-models/create_volume.txt");
-        File downloadFile = $("#raw-url").download();
-        String nameFile = downloadFile.getName();
-        assertEquals(nameFile, "create_volume.txt");
+    void shouldContainsString() throws IOException {
+        String txtFilePath = "src/test/resources/files/1.txt";
+        String expectedContent = "Create and format a volume";
+        String actualData = FileUtils.readFileToString(new File(txtFilePath), "UTF-8");
+
+        assertThat(actualData, containsString(expectedContent));
     }
 }
---
+++ src/test/java/com/deminadarya/tests/FileZipTest.java
@@ -0,0 +1,4 @@
+package com.deminadarya.tests;
+
+public class FileZipTest {
+}
