7dd42502fa7708fa93c2e323a0749ab94b6769dd
daria <darya.demina@simbirsoft.com>
2023-09-17 19:11:36 +0300

    add tests for Docx content

--- src/test/java/com/deminadarya/tests/FileDocTest.java
+++ src/test/java/com/deminadarya/tests/FileDocTest.java
@@ -1,5 +1,8 @@
 package com.deminadarya.tests;
 
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.junit.jupiter.api.Assertions;
@@ -22,11 +25,21 @@ public class FileDocTest {
         Assertions.assertEquals(expectedData, paragraphCount);
     }
 
+    @Test
     void shouldHaveDocxFileContent() throws IOException {
+        XWPFWordExtractor extractor;
+        XWPFDocument document;
         String docxFilePath = "src/test/resources/files/5.docx";
         String expectedData = "Свойства Бэкенда";
 
-        XWPFDocument document = new XWPFDocument(new FileInputStream(docxFilePath));
+        try {
+            document = new XWPFDocument(OPCPackage.open(new FileInputStream(docxFilePath)));
+            extractor = new XWPFWordExtractor(document);
+            String wordText = extractor.getText();
 
+            Assertions.assertTrue(wordText.contains(expectedData));
+        } catch (InvalidFormatException e) {
+            e.printStackTrace();
+        }
     }
 }
